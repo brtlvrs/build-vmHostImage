a4709d4908d8511cbd9801bb3f173df96f71e41c
Bart Lievers <bart@bartlievers.nl>
2016-12-26 13:05:41 +0100

    added comments

--- parameters.ps1
+++ parameters.ps1
@@ -1,6 +1,6 @@
 ﻿@{
     #-- string to filter items in VMware Online Depot
-    VMwareOnlineFilter = "ESXi-6.*-standard"
+    VMwareOnlineFilter = "ESXi-6.0.*-standard" #-- other options are : ESXi-6.0.*-standard, ESXi-6.5.*-standard, ESXi-5.1.*-standard, ESXi-5.5.*-standard
     #-- VMware Online depot URL
     VMwareDepot="https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml"
     #-- are IM project folders siblings 
